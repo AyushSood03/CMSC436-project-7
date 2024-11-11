bb5c59c586d567bfbe2029b5b011c1b0fded247a
wduis1 <123586318+wduis1@users.noreply.github.com>
2024-11-10 22:26:20 -0500

    Created SAXHandler.kt using code from class
    Assumptions:
    -assumes Balloons.kt has a add() method that takes a Balloon
    -assumes Balloons.kt and Balloon.kt have default constructors with no parameters
    -assumes Balloon.kt has a setX(), setY(), and setRadius() functions

--- SAXHandler.kt
+++ SAXHandler.kt
@@ -1,6 +1,56 @@
 package com.example.project7
 
+import android.util.Log
+import org.xml.sax.Attributes
 import org.xml.sax.helpers.DefaultHandler
 
-class SAXHandler: DefaultHandler() {
+class SAXHandler : DefaultHandler() {
+
+    private var balloons : Balloons = Balloons()
+    private var validText : Boolean = false
+    private var currentBalloon : Balloon? = null
+    private var element : String = ""
+
+    override fun startElement(uri: String?, localName: String?, qName: String?, attributes: Attributes?) {
+        super.startElement(uri, localName, qName, attributes)
+        
+        validText = true
+        if(qName != null) {
+            element = qName
+            if(element.equals("balloon")) {
+                currentItem = Balloon()
+            }
+        }
+    }
+
+    override fun endElement(uri: String?, localName: String?, qName: String?) {
+        super.endElement(uri, localName, qName)
+        
+        validText = false
+        if(currentBalloon != null && qName != null && qName.equals("balloon")) {
+            balloons.add(currentBalloon!!)
+        }
+    }
+
+    override fun characters(ch: CharArray?, start: Int, length: Int) {
+        super.characters(ch, start, length)
+        
+        if(ch != null) {
+            var text: String = String(ch!!, start, length)
+            
+            if (currentBalloon != null && element.equals("x") && validText) {
+                currentBalloon!!.setX(text.toInt())
+            }
+            else if (currentBalloon != null && element.equals("y") && validText) {
+              currentBalloon!!.setY(text.toInt())
+            }
+            else if (currentBalloon != null && element.equals("radius" && validText) {
+              currentBalloon!!.setRadius(text.toInt())
+            }
+        }
+    }
+
+    fun getBalloons() : ArrayList<Balloon> {
+         return balloons
+    }
 }
