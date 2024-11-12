e66197274672570d49d7d15f1731dfff8b7672e8
Leo Suber <75337126+LionOfCork@users.noreply.github.com>
2024-11-12 16:43:05 -0500

    Update Balloon.kt
    Added the add() function.
    Added the getBalloons() function.

--- Balloon.kt
+++ Balloon.kt
@@ -1,4 +1,19 @@
 package com.example.project7
 
-class Balloon {
+class Balloons {
+    private lateinit var balloonList : ArrayList<Balloon>
+
+    constructor() {
+        // The constructor should be empty.
+    }
+
+    // Adds a balloon to balloonList.
+    fun add(balloon: Balloon) {
+        balloonList.add(balloon)
+    }
+
+    // Returns the ArrayList of Balloon objects.
+    fun getBalloons() : ArrayList<Balloon> {
+        return balloonList
+    }
 }
