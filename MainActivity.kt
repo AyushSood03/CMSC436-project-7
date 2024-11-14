da458b80cb9779c354b811d43fd0564db96d5263
Leo Suber <75337126+LionOfCork@users.noreply.github.com>
2024-11-14 12:53:38 -0500

    Update MainActivity.kt
    Added functionality to allow for tapping and popping balloons.

--- MainActivity.kt
+++ MainActivity.kt
@@ -3,14 +3,16 @@ package com.example.project7
 
 import android.graphics.Rect
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.text.method.Touch
+import android.util.Log
+import android.view.GestureDetector
+import android.view.MotionEvent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     private lateinit var gameView : GameView
     private lateinit var balloons : Balloons
+    private lateinit var detector : GestureDetector
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,8 +34,28 @@ class MainActivity : AppCompatActivity() {
 
         val rectangle : Rect = Rect(0, 0, 0, 0)
         window.decorView.getWindowVisibleDisplayFrame(rectangle)
+        Log.w("MainActivity", "Top: " + (height - rectangle.top))
 
         gameView = GameView(this, width, height - rectangle.top)
         setContentView(gameView)
+
+        var th : TouchHandler = TouchHandler()
+        detector = GestureDetector(this, th)
+    }
+
+    // Apparently, this is necessary for the taps to be detected.
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        if (event != null)
+            detector.onTouchEvent(event)
+        return super.onTouchEvent(event)
+    }
+
+    inner class TouchHandler : GestureDetector.SimpleOnGestureListener() {
+        // The method that triggers when the user taps on anything.
+        override fun onSingleTapConfirmed(e: MotionEvent): Boolean {
+            gameView.whenClicked(e)
+            updateView()
+            return super.onSingleTapConfirmed(e)
+        }
     }
 }
